efb48040c782ed93051f40e790fec6a217fddee2
ysmt <enuswi67@gmail.com>
2023-01-06 08:50:16 +0900

    Update: 型情報を付加する

--- gcp-firestore/src/index.ts
+++ gcp-firestore/src/index.ts
@@ -1,9 +1,21 @@
-import express from 'express'
+import express, { Application, Request, Response } from 'express'
 
-const app = express()
+const app: Application = express()
+const port: number = 3000
 
-app.get('/', (_, res) => {
-  res.send('Hello, Typescript')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', async (_req: Request, res: Response) => {
+  return res.status(200).send({
+    message: 'Hello, TypeScript'
+  })
 })
 
-app.listen(3000, () => console.log('Server is running'))
+try {
+  app.listen(port, () => {
+    console.log(`dev server running at: http://localhost:${port}`)
+  })
+} catch (e) {
+  console.log(e.message)
+}
