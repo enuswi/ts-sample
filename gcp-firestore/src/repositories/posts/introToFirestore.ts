c57bd76b870d78b4e6e4c924eb658df468f41160
ysmt <enuswi67@gmail.com>
2023-01-07 10:15:56 +0900

    Update: firestore subcollection / 1lineでもかける

--- gcp-firestore/src/repositories/posts/introToFirestore.ts
+++ gcp-firestore/src/repositories/posts/introToFirestore.ts
@@ -6,9 +6,10 @@ export class IntroToFirestoreRepository {
   constructor() {
     const firestore: Firestore = new Firestore()
     //this._document = firestore.doc('posts/intro-to-firestore')
-    this._document = firestore.collection('collection').doc('hoge')
-      .collection('sub-collection').doc('fuga')
-      .collection('sub-sub-collection').doc('hoge-fuga')
+    //this._document = firestore.collection('collection').doc('hoge')
+    //  .collection('sub-collection').doc('fuga')
+    //  .collection('sub-sub-collection').doc('hoge-fuga')
+    this._document = firestore.doc('collection/hoge/subcollection/fuga/subsubcollection/hogefuga')
   }
 
   async insert(_title: string, _body: string) {
