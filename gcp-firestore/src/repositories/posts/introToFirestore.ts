ac5e5e4c09c972041db3787f7f9d5c9a6129ee1e
ysmt <enuswi67@gmail.com>
2023-01-08 14:17:13 +0900

    Update: Firestore戻し（コメントにSubCollectionの書き方を残す）

--- gcp-firestore/src/repositories/posts/introToFirestore.ts
+++ gcp-firestore/src/repositories/posts/introToFirestore.ts
@@ -5,11 +5,11 @@ export class IntroToFirestoreRepository {
 
   constructor() {
     const firestore: Firestore = new Firestore()
-    //this._document = firestore.doc('posts/intro-to-firestore')
+    this._document = firestore.collection('posts').doc('intro-to-firestore')
+    // subCollection例
     //this._document = firestore.collection('collection').doc('hoge')
     //  .collection('sub-collection').doc('fuga')
     //  .collection('sub-sub-collection').doc('hoge-fuga')
-    this._document = firestore.doc('collection/hoge/subcollection/fuga/subsubcollection/hogefuga')
   }
 
   async insert(_title: string, _body: string) {
